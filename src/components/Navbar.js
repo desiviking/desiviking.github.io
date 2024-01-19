127b91a0112ccb21c01441a367476f9c54d6b78e
Vipin Dubey <vipin.kumar.dubey@inmeta.no>
2024-01-19 20:24:57 +0100

    setup gh-pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+const Navbar =()=>{
+      return (
+            <div>
+                  <Link to="/">Home</Link>
+                  <Link to="/blog">Blog</Link>
+                  <Link to="/about">About</Link>
+                  <Link to="/contact">Contact</Link>
+                  <Link to="/privacy-policy">Privacy Policy</Link>
+            </div>
+      )
+}
+export default Navbar;
